862b61419098400cc840cfb15382b26e28823f2f
Piyush Shete <shetepiyush07@gmail.com>
2026-09-21 23:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (44.08%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int inc = 0;
+        int dec = 0;
+        for (int i = 0; i < operations.size(); i++) {
+            if (operations[i] == "++X" || operations[i] == "X++") {
+                inc++;
+            } else {
+                dec++;
+            }
+        }
+        return inc - dec;
+    }
+};
+
+/*class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int X = 0;
+
+        for(int i = 0; i < operations.size(); i++) {
+            if(operations[i] == "++X" || operations[i] == "X++") {
+                X++;
+            } else {
+                X--;
+            }
+        }
+
+        return X;
+    }
+};*/
